73b7958d45b4de8abe01936db77b5eb81a17fa8a
GabrieleLombard01 <Gabriellombardo2001@gmail.com>
2024-01-25 17:31:11 +0100

    post error add

--- app2/src/components/AppProjectCreate.jsx
+++ app2/src/components/AppProjectCreate.jsx
@@ -1,3 +1,29 @@
+import axios from "axios";
+
 export default function ProjectCreate() {
-    return <h1 className="text-danger">Hello</h1>
+  function handleSave() {
+    axios
+      .post("https://localhost:7138/api/projects", {
+        name: "newproject",
+        description: "newproject description",
+      })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  /*
+        Link -> list project
+        <>
+
+            name:
+            description:
+            button onClick  handleSave
+
+    */
+
+  return <button onClick={handleSave}>Post</button>;
 }
